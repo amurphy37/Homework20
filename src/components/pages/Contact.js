688eeda641235e98cce498ad2b8548bf571c226f
Andrew Murphy <amurphy37@gmail.com>
2020-07-11 19:23:41 -0400

    final version

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,10 +2,26 @@ import React from "react"
 
 const Contact = () => (
     <div className="pageDiv">
-        <h1>Contact Page</h1>
-        <p>
-            This will be Contact page content
-        </p>
+       <div className="container">
+            <div className=" section-title center">
+                <h2>Contact</h2>
+                <hr></hr>
+            </div>
+            <div className="col-md-8 col-md-offset-2">
+                <div className=" col-md-4">
+                    <i className="fa fa-map-marker fa-2x"></i>
+                    <p>Los Angeles, CA</p>
+                </div>
+                <div className=" col-md-4">
+                    <i className="fa fa-envelope-o fa-2x"></i>
+                    <p>amurphy37@gmail.com</p>
+                </div>
+                <div className=" col-md-4">
+                    <i className="fa fa-phone fa-2x"></i>
+                    <p>+1 (216) 513-9011</p>
+                </div>
+            </div>
+       </div>
     </div>
 );
 
